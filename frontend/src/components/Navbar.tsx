a29006a9e1fb68d840a0b7b9c31ca146e0b64b3a
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2023-11-01 15:44:50 -0700

    Added Logout to NavBar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            const response = await fetch('/auth/logout', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                navigate('/login');
+            } else {
+                console.error('Failed to log out');
+            }
+        } catch (error) {
+            console.error('Error during logout:', error);
+        }
+    };
+
     return (
         <AppBar position="static">
             <Toolbar>
@@ -13,6 +34,7 @@ const Navbar: React.FC = () => {
                 <Button color="inherit" component={Link} to="/movies">Movies</Button>
                 <Button color="inherit" component={Link} to="/tv">TV</Button>
                 <Button color="inherit" component={Link} to="/suggest">Suggest</Button>
+                <Button color="inherit" onClick={handleLogout}>Logout</Button>
             </Toolbar>
         </AppBar>
     );
