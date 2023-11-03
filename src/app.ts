f5dc27faec505a971612b66a9457fc579023067f
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2023-11-03 15:09:04 -0700

    another change

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 //Initialize session
 app.use(session({
