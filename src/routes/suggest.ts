a945bbd6b2b2e53904594e11240b30b6bd3b16e2
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2023-11-03 17:37:35 -0700

    deleting files

--- src/routes/suggest.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUniqueGenres, getRandomMovieByGenre } from "../controllers/suggest"
-
-const router = express.Router();
-
-router.get('/genres', getUniqueGenres);
-router.get('/movie/:genre', getRandomMovieByGenre);
-
-export default router;
