60f2fc455f16375595307a26cc1baf2b6634af27
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2023-11-04 15:18:57 -0700

    revert to e607115

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,8 +3,7 @@ import { Button, Container, Typography, Box } from '@mui/material';
 
 const LoginPage: React.FC = () => {
     const handleGoogleLogin = () => {
-        window.location.href = '/auth/google';
-        // window.location.href = 'http://localhost:5001/auth/google';
+        window.location.href = 'http://localhost:5001/auth/google';
     };
 
     return (
