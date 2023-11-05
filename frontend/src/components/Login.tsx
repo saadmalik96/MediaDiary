a3686a3699202597b9a448a9a1e70852f92c8676
Saad Malik Paracha <35294802+saadmalik96@users.noreply.github.com>
2023-11-04 17:14:01 -0700

    Update Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,7 +3,7 @@ import { Button, Container, Typography, Box } from '@mui/material';
 
 const LoginPage: React.FC = () => {
     const handleGoogleLogin = () => {
-        window.location.href = 'http://localhost:5001/auth/google';
+        window.location.href = '/auth/google';
     };
 
     return (
